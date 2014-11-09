28a4390ea109260ea422891f01992dc383e9dccb
Junior Tamayo <juni00r.tamayo@gmail.com>
2014-11-09 17:58:13 -0430

    modal

--- initializr/js/main.js
+++ initializr/js/main.js
@@ -1,4 +1,5 @@
 var ar = [0,1,2,3,4,5,6,7,8,9];
+var directo_curr = 1;
 function initialize_poll(q){
 	$('#autoevaluacion').html('');
 	$('#autoevaluacion').append('<div class="row"><div class="col-xs-12 text-center"><h4>Cuestionario</h4></div></div>')
@@ -35,6 +36,23 @@ function initialize_poll(q){
 	
 }
 
+function change_section(section, sense){
+	$('#'+section+'_'+directo_curr).hide();
+	if(sense == 'right'){
+		directo_curr++;
+		if (directo_curr > 3)
+			directo_curr = 1;
+	}
+	else{
+		directo_curr--;
+		if (directo_curr<1){
+			directo_curr = 3;
+		}
+	}
+	$('#'+section+'_'+directo_curr).show();
+
+}
+
 $(function(){
 
 	questions_json = '[{"title":"Los accidentes eléctricos se clasifican en","options":["Electrización por inducción y Contacto eléctrico directo","Contacto eléctrico directo y Contacto eléctrico indirecto","Electrización de cuerpos y Contacto eléctrico indirecto","Electrización por inducción y Electrización de cuerpos"]},{"title":"¿Cuáles de los siguientes son un efecto de accidentes eléctricos por contacto directo?","options":["Electrización y Quemaduras","Embolias y Fiebre","Bajas de azúcar y Tentanización","Ninguna de las anteriores"]},{"title":"¿Qué es la Tentanización?","options":["El paso de corriente por el corazón produciendo un paro circulatorio","La aparición de coágulos en la sangre que pueden obstruir arterias","La incapacidad de separación voluntaria con el punto de contacto eléctrico","El cosquilleo al percibir el paso de la corriente"]},{"title":"¿Cómo podemos prevenir los accidentes eléctricos?","options":["Utilizando cables defectuosos y usando aparatos con carcasas defectuosas","Examinando periodicamente el estado de los cables flexibles","Utilizando varias tomas múltiples","Utilizando instalaciones eléctricas mojadas"]},{"title":"¿Qué hacer en caso de ver que alguien este en contacto con una fuente eléctrica?","options":["Empujarlo para hacerlo reaccionar","Avisar de inmediato al servicio de urgencias","Tomarle las manos para alejarlo de la fuente eléctrica","Darle respiración boca a boca"]},{"title":"Un riesgo eléctrico puede ser visto como:","options":["Choque eléctrico por contacto con elementos en tensión","Quemaduras por choques o arco eléctricos","Incendios o explosiones originados por la electricidad","Todas las anteriores"]},{"title":"Si alguien se está electrocutando y es imposible cortar la electricidad ¿Que deberiamos hacer?","options":["Intentar desenganchar a la persona electrizada con un elemento aislante","Tomarle las manos para alejarlo de la fuente eléctrica","Empujarlo para hacerlo reaccionar","Todas las anteriores"]},{"title":"¿Cuáles de los siguientes no son un efecto de accidentes eléctricos por contacto directo?","options":["Tentanización y Quemaduras","Embolias y Paros cardíacos","Bajas de azúcar y Fiebre","Asfixia y Paro respiratorio"]},{"title":"¿Cómo se definen los accidentes eléctricos por contacto directo?","options":["Es el contacto de personas con partes eléctricamente activas","Es el electrocutamiento por medio de agua","Es el contacto de las personas con algún conductor electrico con fallas de aislamiento","Ninguna de las anteriores"]},{"title":"¿Cómo se definen los accidentes eléctricos por contacto indirecto?","options":["Es el contacto de las personas con elementos conductores puesto accidentalmente bajo tensión por fallos de aislamiento","Es el contacto de las personas con elementos eléctricamente activos","Son explosiones causadas por algún medio eléctrico","Todas las anteriores"]}]';
@@ -56,7 +74,14 @@ $(function(){
 	});
 
 	
+	$('#activar_modal_afecciones').click(function(){
+		$('#afecciones').modal();
+	});
 
-
-	
+	$('.directo_change').click(function(){
+		if ($(this).attr('sense') == 'right')
+			change_section('directo', 'right');
+		else
+			change_section('indirecto', 'left');
+	})
 });
